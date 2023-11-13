7c0c8aeaa32ef0988c6cf32f7cccc96597efc7a9
JAILSON RODRIGUES DE SA <202203883445@alunos.estacio.br>
2023-11-13 19:34:49 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/adpodepet/TelaSelecao.java
@@ -0,0 +1,14 @@
+package com.example.adpodepet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaSelecao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_selecao);
+    }
+}
